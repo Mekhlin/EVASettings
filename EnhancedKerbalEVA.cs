6a24e1fdbf6c7f38e95f4861285474b2e075805a
Christian Tang <git@tcdev.dk>
2020-01-13 23:40:20 +0100

    Ability to disable Surface Sample on pilots and engineers

--- EnhancedKerbalEVA.cs
+++ EnhancedKerbalEVA.cs
@@ -8,6 +8,10 @@ namespace EnhancedEVA
 {
     public class EnhancedKerbalEVA : PartModule
     {
+        private ProtoCrewMember _kerbal;
+        public ProtoCrewMember Kerbal => _kerbal ?? (_kerbal = vessel.GetVesselCrew().FirstOrDefault());
+
+
         [KSPField(guiActive = false, guiActiveEditor = false, guiName = "Kerbal")]
         public string KerbalTraitMenuLabel;
         
@@ -19,6 +23,7 @@ namespace EnhancedEVA
 
         public void Start()
         {
+            if (Kerbal is null) return;
             KerbalTraitField = Fields[nameof(KerbalTraitMenuLabel)];
             KerbalExperienceField = Fields[nameof(KerbalExperienceMenuLabel)];
             ApplyActionWindowSettings();
@@ -29,34 +34,36 @@ namespace EnhancedEVA
         {
             var settings = HighLogic.CurrentGame.Parameters.CustomParams<EnhancedEVAActionSettings>();
             if (!(settings is object)) return;
-            if (!(vessel.GetVesselCrew().FirstOrDefault() is ProtoCrewMember kerbal)) return;
 
             if (settings.ShowTraitLabel && KerbalTraitField is object)
             {
-                KerbalTraitMenuLabel = BuildInfo(kerbal);
-
-                KerbalTraitField.guiActive = true;
-                var sb = new StringBuilder();
-                if (kerbal.isBadass)
-                    sb.Append("Badass ");
-                if (kerbal.veteran)
-                    sb.Append($"{(kerbal.isBadass ? 'v' : 'V')}eteran ");
-
-                sb.Append(kerbal.isBadass || kerbal.veteran ? kerbal.trait.ToLower() : kerbal.trait);
-                KerbalTraitMenuLabel = sb.ToString();
+                KerbalTraitMenuLabel = BuildInfo(Kerbal);
             }
 
             if (settings.ShowExperienceLabel == false || KerbalExperienceField is null) return;
             KerbalExperienceField.guiActive = true;
-            KerbalExperienceMenuLabel = kerbal.experienceLevel.ToString();
+            KerbalExperienceMenuLabel = Kerbal.experienceLevel.ToString();
         }
 
         private void ApplyScienceSettings()
         {
             var settings = HighLogic.CurrentGame.Parameters.CustomParams<EnhancedEVAScienceSettings>();
-            if (settings.RemoveScience == false) return;
-            part.FindModulesImplementing<ModuleScienceExperiment>().ForEach(e => part.RemoveModule(e));
-            part.FindModulesImplementing<ModuleScienceContainer>().ForEach(e => part.RemoveModule(e));
+            if (settings.RemoveScience)
+            {
+                part.FindModulesImplementing<ModuleScienceExperiment>().ForEach(e => part.RemoveModule(e));
+                part.FindModulesImplementing<ModuleScienceContainer>().ForEach(e => part.RemoveModule(e));
+                return;
+            }
+
+            if (!settings.RestrictSurfaceSample) return;
+            if (!(vessel.GetVesselCrew().FirstOrDefault() is ProtoCrewMember kerbal)) return;
+            if (kerbal.trait == KerbalRoster.scientistTrait) return;
+
+            foreach (var scienceExperiment in part.FindModulesImplementing<ModuleScienceExperiment>())
+            {
+                if (scienceExperiment.experimentID != "surfaceSample") continue;
+                part.RemoveModule(scienceExperiment);
+            }
         }
 
         private string BuildInfo(ProtoCrewMember kerbal)
--- Settings/EnhancedEVAScienceSettings.cs
+++ Settings/EnhancedEVAScienceSettings.cs
@@ -1,4 +1,6 @@
-﻿namespace EnhancedEVA.Settings
+﻿using System.Reflection;
+
+namespace EnhancedEVA.Settings
 {
     public class EnhancedEVAScienceSettings : GameParameters.CustomParameterNode
     {
@@ -17,7 +19,15 @@
 
         [GameParameters.CustomParameterUI("Remove all science experiments", gameMode = GameParameters.GameMode.ANY)]
         public bool RemoveScience = false;
+        
+        [GameParameters.CustomParameterUI("Only scientists can take surface samples", gameMode = GameParameters.GameMode.ANY)]
+        public bool RestrictSurfaceSample = false;
 
         #endregion
+
+        public override bool Enabled(MemberInfo member, GameParameters parameters)
+        {
+            return member.Name == nameof(RemoveScience) || RemoveScience == false;
+        }
     }
 }
