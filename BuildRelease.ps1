0ddd2247a2bc5f3ab9c6850c654acd8ac270dd27
Christian Tang <git@tcdev.dk>
2020-03-03 23:05:37 +0100

    BuildRelease.ps1

---
+++ BuildRelease.ps1
@@ -0,0 +1,20 @@
+$productVersion = (Get-Item GameData/EnhancedEVA/Plugins/EnhancedEVA.dll).VersionInfo.ProductVersion
+$7zipPath = "$env:ProgramFiles\7-Zip\7z.exe"
+$source = "./GameData"
+
+if (-not (Test-Path -Path $7zipPath -PathType Leaf)) {
+    throw "7-Zip file '$7zipPath' not found"
+}
+
+if (-not (Test-Path -Path "GameData/EnhancedEVA/Plugins/EnhancedEVA.dll")) {
+    throw "Files not found"
+}
+
+$v = $productVersion.split(".")
+$version = "$($v[0]).$($v[1]).$($v[2])"
+
+Set-Alias 7zip $7zipPath
+
+$target = "release/EnhancedEVA-$($version).zip"
+#7zip a -mx=9 $target $source
+7zip a -tzip $target $source "README.md" "LICENSE"
