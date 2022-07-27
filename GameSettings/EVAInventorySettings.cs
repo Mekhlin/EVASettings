241a3c10256c4c97520860332823774580d5fa8e
Christian Tang <git@tcdev.dk>
2022-07-27 12:15:33 +0200

    Updated compatibility with latest version of KSP

---
+++ GameSettings/EVAInventorySettings.cs
@@ -0,0 +1,29 @@
+﻿using EVASettings.Settings;
+
+namespace EVASettings.GameSettings
+{
+    // ReSharper disable once InconsistentNaming
+    public class EVAInventorySettings : GameParameters.CustomParameterNode
+    {
+        #region Housekeeping
+
+        public override string Title => "EVA Inventory";
+        public override int SectionOrder => 0;
+        public override string Section => SharedSettings.Section;
+        public override string DisplaySection => SharedSettings.DisplaySection;
+        public override GameParameters.GameMode GameMode => SharedSettings.GameMode;
+        public override bool HasPresets => SharedSettings.HasPresets;
+
+        #endregion
+
+        #region Settings
+
+        /// <summary>
+        /// Number of inventory slots available on EVA.
+        /// </summary>
+        [GameParameters.CustomIntParameterUI("Inventory slots", minValue = 2, maxValue = 3, stepSize = 1, toolTip = "Number of inventory slots available on EVA", autoPersistance = true)] 
+        public int InventorySlots = 2;
+
+        #endregion
+    }
+}
